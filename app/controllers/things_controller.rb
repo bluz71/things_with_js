413a6144f9245f641d19699b991ac58be545e682
Dennis B <bluz71@users.noreply.github.com>
2016-03-10 16:25:54 +1100

    Improved Bootstrap styling.

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -21,44 +21,30 @@ class ThingsController < ApplicationController
   def edit
   end
 
-  # POST /things
-  # POST /things.json
   def create
     @thing = Thing.new(thing_params)
 
-    respond_to do |format|
-      if @thing.save
-        format.html { redirect_to @thing, notice: 'Thing was successfully created.' }
-        format.json { render :show, status: :created, location: @thing }
-      else
-        format.html { render :new }
-        format.json { render json: @thing.errors, status: :unprocessable_entity }
-      end
+    if @thing.save
+      flash[:success] = "#{@thing.name} was successfully created"
+      redirect_to @thing
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /things/1
-  # PATCH/PUT /things/1.json
   def update
-    respond_to do |format|
-      if @thing.update(thing_params)
-        format.html { redirect_to @thing, notice: 'Thing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @thing }
-      else
-        format.html { render :edit }
-        format.json { render json: @thing.errors, status: :unprocessable_entity }
-      end
+    if @thing.update(thing_params)
+      flash[:success] = "#{@thing.name} was successfully updated"
+      redirect_to @thing
+    else
+      render :edit
     end
   end
 
-  # DELETE /things/1
-  # DELETE /things/1.json
   def destroy
     @thing.destroy
-    respond_to do |format|
-      format.html { redirect_to things_url, notice: 'Thing was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = "#{@thing.name} was successfully destroyed"
+    redirect_to things_url
   end
 
   # JS response.
